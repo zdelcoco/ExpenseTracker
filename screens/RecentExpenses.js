4c246caee7e0d15acb25a3859982544463a6f03c
Zachary Delcoco <zdelcoco@dmsi.com>
2024-07-29 21:14:10 -0500

    lecture 165 - fetching backend data (GET requests)

--- screens/RecentExpenses.js
+++ screens/RecentExpenses.js
@@ -1,10 +1,19 @@
-import { useContext } from 'react';
+import { useContext, useEffect } from 'react';
 import ExpensesOutput from '../components/ExpensesOutput/ExpensesOutput';
 import { ExpensesContext } from '../store/expenses-context';
 import { getDateMinusDays } from '../util/date';
+import { fetchExpenses } from '../util/http';
 
 function RecentExpenses() {
   const expensesCtx = useContext(ExpensesContext);
+
+  useEffect(() => {
+    async function getExpenses() {
+      const expenses = await fetchExpenses();
+    }
+
+    getExpenses();
+  }, []);
 
   const recentExpenses = expensesCtx.expenses.filter((expense) => {
     const today = new Date();
